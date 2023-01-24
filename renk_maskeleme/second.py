11105acd5659d9d7a5f6c55a1be72b36608e14d9
FURKANKARAMA <62855251+FURKANKARAMA@users.noreply.github.com>
2023-01-25 01:43:19 +0300

    Görünmezlik_pelerini_renk_ayarı
    Görünmezlik pelerini için renk ayarı yaparken pelerinin renginin maskesini ayarlamak için kullanılabilecek uygulama

---
+++ renk_maskeleme/second.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 22:05:56 2023
+
+@author: mavii
+"""
+
+import cv2
+import numpy as np
+
+camera = cv2.VideoCapture(0)
+
+def nothing(x):
+    pass
+cv2.namedWindow("frame")
+
+cv2.createTrackbar("H1","frame",0,359,nothing)
+cv2.createTrackbar("H2","frame",0,359,nothing)
+cv2.createTrackbar("S1","frame",0,255,nothing)
+cv2.createTrackbar("S2","frame",0,255,nothing)
+cv2.createTrackbar("V1","frame",0,255,nothing)
+cv2.createTrackbar("V2","frame",0,255,nothing)
+while camera.isOpened():
+    
+    _, frame = camera.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    H1 = cv2.getTrackbarPos("H1","frame")
+    H2 = cv2.getTrackbarPos("H2","frame")
+    S1 = cv2.getTrackbarPos("S1","frame")
+    S2 = cv2.getTrackbarPos("S2","frame")
+    V1 = cv2.getTrackbarPos("V1","frame")
+    V2 = cv2.getTrackbarPos("V2","frame")
+    
+    lower=np.array([H1,S1,V1])
+    upper = np.array([H2,S2,V2])
+    mask = cv2.inRange(hsv,lower,upper)
+    
+    cv2.imshow("frame",frame)
+    cv2.imshow("hsv",mask)
+    
+    if cv2.waitKey(1)==ord("q"):
+        break
+    
+    
+camera.release()
+cv2.destoyAllWindows()
+       
